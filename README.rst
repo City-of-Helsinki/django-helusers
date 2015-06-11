384da8d854cd6bb9a3154df59e7ca830f0f86bda
Juha Yrjölä <juha.yrjola@iki.fi>
2015-06-11 16:48:32 +0300

    Initial commit

---
+++ README.rst
@@ -0,0 +1,3 @@
+=====
+Django app for City of Helsinki user infrastructure
+=====
---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-helusers',
+    version='0.1',
+    packages=['helusers'],
+    include_package_data=True,
+    license='BSD License',
+    description='Django app for the user infrastructure of the City of Helsinki',
+    long_description=README,
+    url='https://github.com/City-of-Helsinki/helusers',
+    author='Juha Yrjölä',
+    author_email='juha.yrjola@iki.fi',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
