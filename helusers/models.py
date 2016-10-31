5f98add539c3758b4e492ddd2a58d2970f701c9e
Tuomas Suutari <tuomas.suutari@anders.fi>
2016-10-31 16:16:57 +0200

    Fill username from UUID
    Allow creating users without username by filling their username from the
    uuid field if no username is already set.
    
    Also define a new method `set_username_from_uuid` for overriding the
    username with a value generated from the uuid.  This can be used e.g. in
    social logins to force username to the UUID format.

--- helusers/models.py
+++ helusers/models.py
@@ -3,6 +3,9 @@ import logging
 from django.db import models
 from django.contrib.auth.models import AbstractUser as DjangoAbstractUser
 
+from .utils import uuid_to_username
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -11,9 +14,22 @@ class AbstractUser(DjangoAbstractUser):
     department_name = models.CharField(max_length=50, null=True, blank=True)
 
     def save(self, *args, **kwargs):
+        self.clean()
+        return super(AbstractUser, self).save(*args, **kwargs)
+
+    def clean(self):
+        self._make_sure_uuid_is_set()
+        if not self.username:
+            self.set_username_from_uuid()
+        super(AbstractUser, self).clean()
+
+    def _make_sure_uuid_is_set(self):
         if self.uuid is None:
             self.uuid = uuid.uuid1()
-        return super(AbstractUser, self).save(*args, **kwargs)
+
+    def set_username_from_uuid(self):
+        self._make_sure_uuid_is_set()
+        self.username = uuid_to_username(self.uuid)
 
     class Meta:
         abstract = True
