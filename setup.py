f74c2b617b8fddecccaf0cd1fc114f141a61b3e8
Tuomas Suutari <tuomas.suutari@anders.fi>
2016-10-31 16:37:20 +0200

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-helusers',
-    version='0.1',
+    version='0.2',
     packages=['helusers'],
     include_package_data=True,
     license='BSD License',
