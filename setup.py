976317ef2b1e77d6f5c90c8717a6d3987e76e686
Juha Yrjölä <juha.yrjola@iki.fi>
2017-06-08 18:15:09 +0300

    Remove Yletunnus provider and move to City-of-Helsinki/tunnistamo

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-helusers',
-    version='0.2.5',
+    version='0.3.0',
     packages=['helusers'],
     include_package_data=True,
     license='BSD License',
@@ -34,6 +34,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
