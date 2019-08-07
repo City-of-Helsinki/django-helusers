c9630058f0b1bb00c00817f7a7feb7791db1e8ce
Juha Yrjölä <juha.yrjola@iki.fi>
2019-08-07 15:38:23 +0300

    Bump version to 0.4.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-helusers',
-    version='0.4.5',
+    version='0.4.6',
     packages=['helusers'],
     include_package_data=True,
     license='BSD License',
