62ec7d8ed62fde88726de476fa7beeb135aaa8bd
Juha Yrjölä <juha.yrjola@iki.fi>
2020-01-13 12:22:59 +0200

    Version 0.5.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-helusers',
-    version='0.5.3',
+    version='0.5.4',
     packages=['helusers'],
     include_package_data=True,
     license='BSD License',
