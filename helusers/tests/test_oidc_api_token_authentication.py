b3c0872729256b69e0ca4203c1c94a4603c28206
Aki Koskinen <aki@secondlion.fi>
2020-10-20 09:17:57 +0300

    Add one test for helusers.oidc.ApiTokenAuthentication
    This tests the happy path case, where the authentication passes. It's
    just an example how this class could be tested.
    
    The RSA keys found from keys.py are generated just for this purpose so
    it's not a biggy that there are both the public and private keys in the
    same file clearly readable.

---
+++ helusers/tests/test_oidc_api_token_authentication.py
@@ -0,0 +1,49 @@
+import json
+import time
+
+import pytest
+from jose import jwt
+
+from helusers.oidc import ApiTokenAuthentication
+
+from .keys import rsa_key
+
+
+ISSUER = "test_issuer"
+
+class TestableApiTokenAuthentication(ApiTokenAuthentication):
+    @property
+    def oidc_config(self):
+        return {
+            "issuer": ISSUER,
+        }
+
+    def jwks_data(self):
+        return json.dumps({"keys": [rsa_key.public_key_jwk]})
+
+
+@pytest.mark.django_db
+def test_valid_jwt_is_accepted(rf):
+    sut = TestableApiTokenAuthentication()
+
+    unix_timestamp_now = int(time.time())
+
+    user_uuid = "b7a35517-eb1f-46c9-88bf-3206fb659c3c"
+    jwt_data = {
+        "iss": ISSUER,
+        "aud": "test_audience",
+        "iat": unix_timestamp_now - 10,
+        "exp": unix_timestamp_now + 1000,
+        "sub": user_uuid,
+    }
+
+    encoded_jwt = jwt.encode(
+        jwt_data, key=rsa_key.private_key_pem, algorithm=rsa_key.jose_algorithm
+    )
+
+    request = rf.get("/path", HTTP_AUTHORIZATION=f"Bearer {encoded_jwt}")
+
+    (user, auth) = sut.authenticate(request)
+
+    assert user.uuid == user_uuid
+    assert auth.user == user
