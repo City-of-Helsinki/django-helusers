f5c57241c50f970e38f17461591f6ac00a5fe0fe
Juha Louhiranta <juha.louhiranta@anders.fi>
2021-06-02 12:38:38 +0300

    Add HelusersConfig.default_auto_field for Django 3.2
    Projects using Django >=3.2 will define DEFAULT_AUTO_FIELD
    which for new projects will be BigAutoField. To avoid
    unwanted migrations in the future default_auto_field will
    be configured to 'django.db.models.AutoField'.
    
    Refs https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys

--- helusers/apps.py
+++ helusers/apps.py
@@ -6,6 +6,7 @@ from django.contrib.admin.apps import AdminConfig
 class HelusersConfig(AppConfig):
     name = 'helusers'
     verbose_name = _("Helsinki Users")
+    default_auto_field = "django.db.models.AutoField"
 
 
 class HelusersAdminConfig(AdminConfig):
