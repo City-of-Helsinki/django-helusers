95eb73ce7645ae6275fbb958ec803ce521b16198
Aki Koskinen <aki@secondlion.fi>
2021-06-10 10:59:35 +0300

    Check configuration before specifying urlpatterns
    If the configuration is incorrect, it doesn't make sense to specify the
    URL patterns in that case.

--- helusers/urls.py
+++ helusers/urls.py
@@ -7,17 +7,21 @@ from . import views
 
 app_name = "helusers"
 
-urlpatterns = [
-    path("logout/", views.LogoutView.as_view(), name="auth_logout"),
-    path(
-        "logout/complete/",
-        views.LogoutCompleteView.as_view(),
-        name="auth_logout_complete",
-    ),
-    path("login/", views.LoginView.as_view(), name="auth_login"),
-]
+urlpatterns = []
 
 if not settings.LOGOUT_REDIRECT_URL:
     raise ImproperlyConfigured(
         "You must configure LOGOUT_REDIRECT_URL to use helusers views."
     )
+
+urlpatterns.extend(
+    [
+        path("logout/", views.LogoutView.as_view(), name="auth_logout"),
+        path(
+            "logout/complete/",
+            views.LogoutCompleteView.as_view(),
+            name="auth_logout_complete",
+        ),
+        path("login/", views.LoginView.as_view(), name="auth_login"),
+    ]
+)
