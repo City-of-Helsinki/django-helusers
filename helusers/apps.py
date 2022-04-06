34eb331d31783e8668503bfaef6c7d13e49e4ff1
Daniel S. Kislyuk <daniel.kislyuk@gmail.com>
2022-04-06 10:39:39 +0300

    Fix default AppConfig
    Importing AdminConfig directly results in ''helusers.apps' declares more than one default AppConfig' error in Django 3.2

--- helusers/apps.py
+++ helusers/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
-from django.contrib.admin.apps import AdminConfig
+from django.contrib.admin import apps
 
 
 class HelusersConfig(AppConfig):
@@ -9,5 +9,5 @@ class HelusersConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
 
 
-class HelusersAdminConfig(AdminConfig):
+class HelusersAdminConfig(apps.AdminConfig):
     default_site = 'helusers.admin_site.AdminSite'
