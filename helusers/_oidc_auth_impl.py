9b1d5be1d1da64dabc8676adbca20c312b8673c0
Mikko Keskinen <mikko.keskinen@newthings.co>
2023-06-21 08:11:31 +0300

    Remove drf-oidc-auth requirement when using ApiTokenAuthentication
    drf-oidc-auth has some bugs and problems with token lifetime and
    claims validation. It's also better to have only one way of validating
    tokens between RequestJWTAuthentication and ApiTokenAuthentication.
    
    Base class in ApiTokenAuthentication is changed from
    oidc_auth.authentication.JSONWebTokenAuthentication to
    rest_framework.authentication.BaseAuthentication. Some methods from
    JSONWebTokenAuthentication are kept for backwards compatibility but
    the token validation is now handled by the JWT class.
    
    See the CHANGLELOG for more detailed notes.

--- helusers/_oidc_auth_impl.py
+++ helusers/_oidc_auth_impl.py
@@ -1,57 +1,35 @@
 import logging
-import requests
-from authlib.jose import JoseError
-from authlib.jose.errors import ExpiredTokenError
+
+from cachetools.func import ttl_cache
 from django.utils.encoding import smart_str
-from django.utils.functional import cached_property
 from django.utils.translation import gettext as _
-from oidc_auth.authentication import JSONWebTokenAuthentication
-from oidc_auth.util import cache
-from rest_framework.authentication import get_authorization_header
+from jose import JWTError
+from rest_framework.authentication import BaseAuthentication, get_authorization_header
 from rest_framework.exceptions import AuthenticationFailed
 
 from .authz import UserAuthorization
+from .jwt import JWT, ValidationError
 from .settings import api_token_auth_settings
 from .user_utils import get_or_create_user
 
 logger = logging.getLogger(__name__)
 
 
-class ApiTokenAuthentication(JSONWebTokenAuthentication):
+class ApiTokenAuthentication(BaseAuthentication):
+    www_authenticate_realm = "api"
+
     def __init__(self, settings=None, **kwargs):
         self.settings = settings or api_token_auth_settings
         super(ApiTokenAuthentication, self).__init__(**kwargs)
 
     @property
-    # This method is used if the drf-oidc-auth dependecy version is 1.0.0 or greater
-    def claims_options(self):
-        _claims_options = super().claims_options
-
-        audiences = self.settings.AUDIENCE
-        if isinstance(audiences, str):
-            audiences = [self.settings.AUDIENCE]
-
-        _claims_options["aud"] = {
-            "essential": True,
-            "values": audiences
-        }
-        return _claims_options
-
-    @cached_property
     def auth_scheme(self):
-        return self.settings.AUTH_SCHEME or 'Bearer'
+        return self.settings.AUTH_SCHEME or "Bearer"
 
-    @property
-    def oidc_config(self):
-        return self.get_oidc_config()
-
-    @cache(ttl=api_token_auth_settings.OIDC_CONFIG_EXPIRATION_TIME)
-    def get_oidc_config(self):
-        issuer = self.settings.ISSUER
-        if not isinstance(issuer, str):
-            issuer = issuer[0]
-        url = issuer + '/.well-known/openid-configuration'
-        return requests.get(url).json()
+    @ttl_cache(ttl=api_token_auth_settings.OIDC_CONFIG_EXPIRATION_TIME)
+    def get_oidc_config(self, issuer):
+        from helusers.oidc import OIDCConfig
+        return OIDCConfig(issuer)
 
     def authenticate(self, request):
         jwt_value = self.get_jwt_value(request)
@@ -60,21 +38,10 @@ class ApiTokenAuthentication(JSONWebTokenAuthentication):
 
         try:
             payload = self.decode_jwt(jwt_value)
-        except AuthenticationFailed as e:
-            logger.debug("Invalid token signature")
-            raise
-
-        # Some OPs may provide the "amr" incorrectly as a string, while the
-        # specification dictates it must be an array of strings. Fix that here.
-        if isinstance(payload.get("amr"), str):
-            payload["amr"] = [payload["amr"]]
-            logger.debug(
-                'Modified "amr" claim to be an array of strings instead of a string.'
-            )
+        except JWTError:
+            return None
 
         logger.debug("Token payload decoded as: {}".format(payload))
-
-        self.validate_claims(payload)
 
         user_resolver = self.settings.USER_RESOLVER  # Default: resolve_user
         try:
@@ -83,14 +50,28 @@ class ApiTokenAuthentication(JSONWebTokenAuthentication):
             raise AuthenticationFailed(str(e)) from e
         auth = UserAuthorization(user, payload, self.settings)
 
-        if self.settings.REQUIRE_API_SCOPE_FOR_AUTHENTICATION:
-            api_scope = self.settings.API_SCOPE_PREFIX
-            if not auth.has_api_scope_with_prefix(api_scope):
-                raise AuthenticationFailed(
-                    _("Not authorized for API scope \"{api_scope}\"")
-                    .format(api_scope=api_scope))
+        return user, auth
 
-        return (user, auth)
+    def decode_jwt(self, jwt_value):
+        jwt = JWT(jwt_value, settings=self.settings)
+
+        try:
+            jwt.validate_issuer()
+        except ValidationError as e:
+            raise AuthenticationFailed(str(e)) from e
+
+        keys = self.get_oidc_config(jwt.issuer).keys()
+        try:
+            jwt.validate(keys, self.settings.AUDIENCE)
+            jwt.validate_api_scope()
+            jwt.validate_session()
+            self.validate_claims(jwt.claims)
+        except ValidationError as e:
+            raise AuthenticationFailed(str(e)) from e
+        except Exception:
+            raise AuthenticationFailed("JWT verification failed.")
+
+        return jwt.claims
 
     def get_jwt_value(self, request):
         auth = get_authorization_header(request).split()
@@ -111,23 +92,12 @@ class ApiTokenAuthentication(JSONWebTokenAuthentication):
         return auth[1]
 
     def validate_claims(self, id_token):
-        try:
-            id_token.validate()
-        except ExpiredTokenError:
-            msg = _('Invalid Authorization header. JWT has expired.')
-            raise AuthenticationFailed(msg)
-        except JoseError as e:
-            msg = _(str(type(e)) + str(e))
-            raise AuthenticationFailed(msg)
+        """Not in use. Only for backwards compatibility"""
 
     def authenticate_header(self, request):
         return '{auth_scheme} realm="{realm}"'.format(
             auth_scheme=self.auth_scheme,
             realm=self.www_authenticate_realm)
-
-    # This method is only used if the drf-oidc-auth dependecy version is less than 1.0.0
-    def get_audiences(self, api_token):
-        return {self.settings.AUDIENCE}
 
 
 def resolve_user(request, payload):
