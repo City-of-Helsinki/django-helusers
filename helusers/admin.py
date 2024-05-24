8135f14c03ac55994d9af651478bfe2021913bed
Matti Eiden <matti.eiden@anders.com>
2024-05-24 10:48:04 +0300

    fix: improve ModelAdmins for ADGroupMapping and ADGroup
    Adds new columns, sorting, searching, filtering and default ordering.
    
    Refs: RAT-173

--- helusers/admin.py
+++ helusers/admin.py
@@ -1,15 +1,53 @@
 from django.conf import settings
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from .models import ADGroup, ADGroupMapping, OIDCBackChannelLogoutEvent
 
 
 @admin.register(ADGroupMapping)
 class ADGroupMappingAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        "get_str",
+        "get_ad_group_name",
+        "get_ad_group_display_name",
+        "group",
+    ]
+    list_filter = ("group",)
+    search_fields = (
+        "group__name",
+        "ad_group__name",
+        "ad_group__display_name",
+    )
+
+    ordering = ("ad_group__display_name", "group")
+
+    @admin.display(description="Mapping")
+    def get_str(self, obj):
+        return str(obj)
+
+    @admin.display(description=_("AD group name"), ordering="ad_group__name")
+    def get_ad_group_name(self, obj):
+        return obj.ad_group.name
+
+    @admin.display(
+        description=_("AD group display name"), ordering="ad_group__display_name"
+    )
+    def get_ad_group_display_name(self, obj):
+        return obj.ad_group.display_name
 
 
-admin.site.register(ADGroup)
+@admin.register(ADGroup)
+class ADGroupAdmin(admin.ModelAdmin):
+    list_display = [
+        "display_name",
+        "name",
+    ]
+    search_fields = (
+        "display_name",
+        "name",
+    )
+    ordering = ("display_name", "name")
 
 
 if getattr(settings, "HELUSERS_BACK_CHANNEL_LOGOUT_ENABLED", False):
